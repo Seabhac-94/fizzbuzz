c73828f44c1e01b170350d483add4af5de007ff6
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-05-15 11:27:37 +0200

    completed for java

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
 // Write a program that prints the numbers from 1 to 100 and for multipples of '3' print 'Fizz' instead of the number, and for the multiples of '5' print 'Buzz'.
+//1,2,Fizz,4,Buzz,Fizz....Buzz
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //psuedo code
+        for (int count = 1; count <= 100; count++) {
+
+            if (count % 3 == 0 && count % 5 == 0 ){
+                System.out.println("FizzBuzz");
+            }
+            else if (count % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (count % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(count);
+            }
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+## Overview
+
+Basic interview question - FizzBuzz.
+
+Requirements: Write a program that prints the numbers from 1 to 100 and for multipples of '3' print 'Fizz' instead of the number, and for the multiples of '5' print 'Buzz'.
+
+Expected output:
+
+1<br>
+2<br>
+Fizz<br>
+4<br>
+Buzz<br>
+Fizz<br>
+....Buzz
