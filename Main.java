945ec6bcaa8d24c32f1c8e68f68f4573df599e3d
Zachary Allen <zac_allen@Apples-MacBook-Pro.local>
2021-05-15 11:14:54 +0200

    intial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Write a program that prints the numbers from 1 to 100 and for multipples of '3' print 'Fizz' instead of the number, and for the multiples of '5' print 'Buzz'.
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
